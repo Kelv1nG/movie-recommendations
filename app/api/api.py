61f4eef78df04c68be788b5f3ab0fdaf28b16992
Kelv1nG <kelvinmaglipon@gmail.com>
2024-05-04 15:06:51 +0800

    minor changes

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,7 @@
-from app.api.controllers.movie_scraper import MovieScraperController
 from litestar import Router
+
+from app.api.controllers.movie_scraper import MovieScraperController
 
 router = Router(path="/api", route_handlers=[])
 
 router.register(MovieScraperController)
-
--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from litestar import Litestar
 from app.api.api import router as api_router
 
 app = Litestar(
-    debug=True
+    debug=True,
 )
 
 app.register(api_router)
